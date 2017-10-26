ee6e57c1b6216b4c246e3200f64b3b849760efc6
Rafael Molla <rafaelmolladouglas@gmail.com>
2017-10-25 21:24:10 -0300

    Share on twitter added

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -4,6 +4,8 @@ request.send();
 
 var quoteNumber = 0;
 var colors = ['#f1c40f', '#34495e', '#2ecc71', '#e74c3c', '#d35400', '#3498db'];
+var twitterWebIntent = "https://twitter.com/intent/tweet?text=";
+
 request.onreadystatechange = function() {
 	if (request.readyState == 4) {
 		if (request.status == 200) {
@@ -13,6 +15,10 @@ request.onreadystatechange = function() {
 			// Show first quote and its author as default
 			document.getElementById("quote").innerHTML = data[quoteNumber]["quote"];
 			document.getElementById("author").innerHTML = data[quoteNumber]["author"];
+
+			// First quote as default for sharing
+			var shareQuote = twitterWebIntent + data[quoteNumber]["quote"] + " by " + data[quoteNumber]["author"] + "&hashtags=randomquotes"; 
+
 
 			// If user clicks new quote buttton, move to next quote in data
 			document.getElementById("new-quote").onclick = function() {
@@ -28,14 +34,19 @@ request.onreadystatechange = function() {
 				document.getElementById("quote").innerHTML = data[quoteNumber]["quote"];
 				document.getElementById("author").innerHTML = data[quoteNumber]["author"];
 
+				// Update twitter share url when user clicks new quote
+				shareQuote = twitterWebIntent + data[quoteNumber]["quote"] + " by " + data[quoteNumber]["author"] + "&hashtags=randomquotes";
+
 				// Change background color when user clicks new quote
 				var newColor = Math.floor(Math.random() * 6);
 				document.body.style.background = colors[newColor];
 
 			}
 
-
-
+			// Add href to twitter link
+			document.getElementById("twitter-share").onclick = function() {
+				this.href=shareQuote;
+			}
 
 	} else {
 			console.log("An error occured");
